47d85cd4191eb959e05fac98608420761fbe0064
Sachin094 <crazy.sachin10@gmail.com>
2015-07-01 10:26:30 +0530

    Just added Mohan's work
    Added Mohan's work

---
+++ javalearning/src/Basics.java
@@ -0,0 +1,18 @@
+
+public class Basics {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Mohan m= new Mohan();
+		
+		int sum=m.add(15, 68);
+		
+		System.out.println(sum);
+
+	}
+
+}
